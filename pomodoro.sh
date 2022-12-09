4a6a8623f36b17c86b33cfdc58e19012749c3252
yigit-cankurtaran <yigitcankurtaranyc@gmail.com>
2022-12-09 18:09:27 +0300

    Rewrote the script, added a timer, will test if it works.

--- pomodoro.sh
+++ pomodoro.sh
@@ -1,56 +1,70 @@
-# Set the amount of time for each pomodoro (in minutes)
+#!/bin/bash
+
+# Set the length of each pomodoro in minutes
 pomodoro_length=25
 
-# Set the length of the break (in minutes)
+# Set the number of pomodoros before a break
+pomodoros_per_break=4
+
+# Set the length of each break in minutes
 break_length=5
 
-# Set the number of pomodoros before taking a longer break
-pomodoros_before_long_break=4
-
-# Set the length of the long break (in minutes)
-long_break_length=15
-
-# Set the number of pomodoros completed to 0
+# Initialize the pomodoro and break counters
 pomodoros_completed=0
+breaks_taken=0
 
-# Set the path to the sound file
-sound_file="./alarm.wav"
+# Get the pomodoro length in seconds
+pomodoro_length_sec=$((pomodoro_length * 60))
+
+# Start the timer
+echo "Starting pomodoro for $pomodoro_length minutes..."
+for ((i = pomodoro_length_sec; i >= 0; i--)); do
+  # Calculate the minutes and seconds remaining
+  minutes=$((i / 60))
+  seconds=$((i % 60))
+
+  # Print the remaining time
+  printf "\rTime remaining: %02d:%02d" $minutes $seconds
+
+  # Sleep for 1 second
+  sleep 1
+done
+
+# Print a newline after the timer is done
+echo ""
 
 while true; do
-  # Print a message to let the user know a pomodoro is starting
-  printf "Starting pomodoro %d\n" $pomodoros_completed
+  # Display the remaining time for the current pomodoro or break
+  if [[ pomodoros_completed -gt 0 ]]; then
+    if [[ $((pomodoros_completed % pomodoros_per_break)) -eq 0 ]]; then
+      # It's time for a break
+      minutes_remaining=$break_length
+      echo "Taking a break for $minutes_remaining minutes..."
+    else
+      # It's time for a pomodoro
+      minutes_remaining=$pomodoro_length
+      echo "Working for $minutes_remaining minutes..."
+    fi
+  fi
 
-  # Sleep for the length of a pomodoro
-  sleep $(($pomodoro_length * 60))
+  # Sleep for one minute and then update the remaining time
+  sleep 60
+  minutes_remaining=$((minutes_remaining - 1))
 
-  # Play the sound to signal the end of the pomodoro
-  # This works for Linux. On Mac use the "afplay" command instead.
-  aplay $sound_file
-
-  # Increment the number of pomodoros completed
-  pomodoros_completed=$(($pomodoros_completed + 1))
-
-  # Check if it's time for a long break
-  if [ $pomodoros_completed -eq $pomodoros_before_long_break ]; then
-    # Print a message to let the user know a long break is starting
-    printf "Starting long break\n"
-
-    # Sleep for the length of the long break
-    sleep $(($long_break_length * 60))
-
-    # Play the sound to signal the end of the long break
-    aplay $sound_file
-
-    # Reset the number of pomodoros completed
-    pomodoros_completed=0
-  else
-    # Print a message to let the user know a short break is starting
-    printf "Starting short break\n"
-
-    # Sleep for the length of the short break
-    sleep $(($break_length * 60))
-
-    # Play the sound to signal the end of the short break
-    aplay $sound_file
+  # Check if the current pomodoro or break is finished
+  if [ $minutes_remaining -eq 0 ]; then
+    if [ $((pomodoros_completed % pomodoros_per_break)) -eq 0 ]; then
+      # A break just ended
+      breaks_taken=$((breaks_taken + 1))
+      echo "Break is over. Time to get back to work!"
+      aplay "./alarm.wav"
+    else
+      # A pomodoro just ended
+      pomodoros_completed=$((pomodoros_completed + 1))
+      echo "Pomodoro is over. Time for a break!"
+      # Play a sound to indicate that the pomodoro is over
+      # Replace "sound.mp3" with the path to the desired sound file
+      aplay "./alarm.wav"
+    fi
   fi
 done
